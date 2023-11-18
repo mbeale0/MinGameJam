69c20ad233e3335b2643754d60bf1e0f67749763
MasonBeale <mbeale@cedarville.edu>
2023-11-18 16:13:09 -0500

    Added sprites

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Sprite walkRight = null;
+    [SerializeField] private Sprite walkLeft = null;
+    [SerializeField] private Sprite standing = null;
     private GameObject[] kittens = new GameObject[5];
     public GameObject kitten;
+    private bool isMoving = false;
+    private bool inWalkingAnimation = false;
 
     private void Start()
     {
@@ -19,21 +24,33 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if(isMoving && !inWalkingAnimation){
+            inWalkingAnimation = true;
+            StartCoroutine(AnimateWalking());
+        }
         if(Input.GetKey(KeyCode.W))
         {
+            isMoving = true;
             transform.Translate(0, 10 * Time.deltaTime, 0);
         }
         else if(Input.GetKey(KeyCode.S))
         {
+            isMoving = true;
             transform.Translate(0, -10 * Time.deltaTime, 0);
         }
         else if(Input.GetKey(KeyCode.A))
         {
+            isMoving = true;
             transform.Translate(-10 * Time.deltaTime, 0, 0);
         }
         else if(Input.GetKey(KeyCode.D))
         {
+            isMoving = true;
             transform.Translate(10 * Time.deltaTime, 0, 0);
+        }
+        else{
+            isMoving = false;
+            GetComponent<SpriteRenderer>().sprite = standing;
         }
 
         if (Input.GetMouseButtonDown(0))
@@ -53,5 +70,12 @@ public class PlayerController : MonoBehaviour
         //TODO: Lose
     }   
 
+    private IEnumerator AnimateWalking(){
+        GetComponent<SpriteRenderer>().sprite = walkRight;
+        yield return new WaitForSeconds(.2f);
+        GetComponent<SpriteRenderer>().sprite = walkLeft;
+        yield return new WaitForSeconds(.2f);
+        inWalkingAnimation = false;
+    }
     
 }
