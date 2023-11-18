14f5a1ee890466921538d570175ad9bfe3506e00
Brandon <brandonbarker169@gmail.com>
2023-11-18 17:01:14 -0500

    Fountain sprite

--- Assets/Scripts/FountainofMilk.cs
+++ Assets/Scripts/FountainofMilk.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class FountainofMilk : MonoBehaviour
 {
+    [SerializeField] private Sprite kat = null;
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Kitten"))
+        if (other.gameObject.CompareTag("kitten"))
         {
             if (other.GetComponent<kittenMovement>().state >= 3)
             {
+                other.GetComponent<SpriteRenderer>().sprite = kat;
+                other.GetComponent<SpriteRenderer>().enabled = true;
                 other.GetComponent<kittenMovement>().state = 1;
             }
         }
